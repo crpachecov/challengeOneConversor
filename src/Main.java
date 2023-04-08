da77bb434556873159b23be0069ee95718f2113f
crpachecov <crpacheco@unicesar.edu.co>
2023-04-08 08:05:56 -0500

    finished

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,20 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
+        boolean continuar = true;
+
         Object[] options = {"Convertir Divisas", "Convertir Temperaturas", "Salir"};
         int elige = JOptionPane.showOptionDialog(null, "Elige una opción",
                 "Conversor One", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE,
                 null, options, options[0]);
 
-        //Cuando desee continuar que regrese al menu principal
+        while (continuar) {
+            switch (elige) {
+                case 0:
+                    String[] opcionesDivisas = {"Pesos a Dólares", "Pesos a Euros",
+                            "Pesos a Libras", "Pesos a Yen", "Pesos a Won"};
+                    Conversion conversionDivisas;
 
-        switch (elige) {
-            case 0:
-                String[] opcionesDivisas = {"Pesos a Dólares", "Pesos a Euros",
-                        "Pesos a Libras", "Pesos a Yen", "Pesos a Won"};
-                Conversion conversionDivisas;
-                boolean continuarPrograma = true;
-                while (continuarPrograma) {
                     int cambio = JOptionPane.showOptionDialog(null, "Elige una opción",
                             "Conversor de Divisas", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE,
                             null, opcionesDivisas, opcionesDivisas[0]);
@@ -39,23 +39,26 @@ public class Main {
                             JOptionPane.YES_NO_CANCEL_OPTION);
 
                     if (confirma == JOptionPane.NO_OPTION || confirma == JOptionPane.CANCEL_OPTION) {
-                        continuarPrograma = false;
+                        continuar = false;
                         JOptionPane.showMessageDialog(null, "Programa finalizado");
+                    } else {
+                        elige = JOptionPane.showOptionDialog(null, "Elige una opción",
+                                "Conversor One", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE,
+                                null, options, options[0]);
                     }
-                }
-                break;
-            case 1:
-                String[] opcionesTemperaturas = {"Celsius a Fahrenheit", "Celsius a Kelvin",
-                        "Fahrenheit a Celsius", "Fahrenheit a Kelvin", "Kelvin a Celsius",
-                        "Kelvin a Fahrenheit"};
-                Conversion conversionTemperaturas;
-                boolean continuarPrograma2 = true;
-                while (continuarPrograma2) {
-                    int cambio = JOptionPane.showOptionDialog(null, "Elige una opción",
+
+                    break;
+                case 1:
+                    String[] opcionesTemperaturas = {"Celsius a Fahrenheit", "Celsius a Kelvin",
+                            "Fahrenheit a Celsius", "Fahrenheit a Kelvin", "Kelvin a Celsius",
+                            "Kelvin a Fahrenheit"};
+                    Conversion conversionTemperaturas;
+
+                    cambio = JOptionPane.showOptionDialog(null, "Elige una opción",
                             "Conversor de Temperaturas", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE,
                             null, opcionesTemperaturas, opcionesTemperaturas[0]);
 
-                    String input = JOptionPane.showInputDialog("Introduce la cantidad de grados a convertir");
+                    input = JOptionPane.showInputDialog("Introduce la cantidad de grados a convertir");
                     double grados = 0;
 
                     try {
@@ -68,23 +71,25 @@ public class Main {
                         JOptionPane.showMessageDialog(null, "Introduce un número válido");
                     }
 
-                    int confirma = JOptionPane.showConfirmDialog(null,
+                    confirma = JOptionPane.showConfirmDialog(null,
                             "¿Desea continuar con el programa?", "Confirmar",
                             JOptionPane.YES_NO_CANCEL_OPTION);
 
                     if (confirma == JOptionPane.NO_OPTION || confirma == JOptionPane.CANCEL_OPTION) {
-                        continuarPrograma2 = false;
+                        continuar = false;
                         JOptionPane.showMessageDialog(null, "Programa finalizado");
+                    } else {
+                        elige = JOptionPane.showOptionDialog(null, "Elige una opción",
+                                "Conversor One", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE,
+                                null, options, options[0]);
                     }
-                }
-                break;
+                    break;
 
-            case 2:
-                JOptionPane.showMessageDialog(null, "Programa finalizado");
-                System.exit(0);
-                break;
-
+                case 2:
+                    JOptionPane.showMessageDialog(null, "Programa finalizado");
+                    System.exit(0);
+                    break;
+            }
         }
-
     }
 }
